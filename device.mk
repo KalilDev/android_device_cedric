7036968f8d5609286b0079c0f832747313f0d996
Pedro Kalil <kalilgamer0@gmail.com>
2018-05-17 20:16:12 +0000

    device: Include missing dependencies for the power HIDL hal

--- device.mk
+++ device.mk
@@ -275,7 +275,9 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service-qti
+    android.hardware.power@1.1-service-qti \
+    android.hardware.power@1.0-impl \
+    android.hardware.power@1.0-service
 
 # Qualcomm
 PRODUCT_COPY_FILES += \
